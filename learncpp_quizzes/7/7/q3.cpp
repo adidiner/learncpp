dfe22209bf77bab36af04d63e535405b01a9c7f5
Adi Dinerstein <adi.dinerstein@gmail.com>
2021-03-29 20:08:02 +0300

    chapter 7.7 quiz q3

---
+++ learncpp_quizzes/7/7/q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    for (int outer {5}; outer >= 1; --outer) {
+        for (int inner {outer}; inner >= 1; --inner) {
+            std::cout << inner << ' ';
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
