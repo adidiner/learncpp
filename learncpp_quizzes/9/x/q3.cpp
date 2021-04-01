b39ee217da4af9a28368beae2f3d80cae015fb02
Adi Dinerstein <adi.dinerstein@gmail.com>
2021-04-01 15:01:05 +0300

    chapter 9.x quiz q3

---
+++ learncpp_quizzes/9/x/q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void swap(int& a, int& b) {
+    int temp{a};
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int a{1}, b{7};
+    std::cout << "a = " << a << ", b = " << b << '\n';
+    swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << '\n';
+    return 0;
+}
