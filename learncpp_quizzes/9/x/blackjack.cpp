488c0d1cbbfdaa22feaa4239ed63f5ddaa1b7a82
Adi Dinerstein <adi.dinerstein@gmail.com>
2021-04-01 22:15:26 +0300

    blackjack

--- learncpp_quizzes/9/x/blackjack.cpp
+++ learncpp_quizzes/9/x/blackjack.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <ctime>
 #include <iostream>
+#include <limits>
 #include <random>
 
 enum class Rank {
@@ -36,9 +37,11 @@ struct Card {
     Suit suit{};
 };
 
-constexpr std::size_t deck_size{52};
+constexpr size_t deck_size{52};
+constexpr int blackjack{21};
+
 using deck_t = std::array<Card, deck_size>;
-using index_t = deck_t::size_type;
+using hand_t = std::vector<Card>;
 
 void print_card(const Card& card) {
     char rank{};
@@ -83,7 +86,7 @@ deck_t create_deck() {
     int suits{static_cast<int>(Suit::max_suits)};
     int ranks{static_cast<int>(Rank::max_ranks)};
 
-    index_t c{};
+    size_t c{};
     for (int s{}; s < suits; ++s) {
         for (int r{}; r < ranks; ++r) {
             deck[c].rank = static_cast<Rank>(r);
@@ -107,10 +110,102 @@ void shuffle_deck(deck_t& deck) {
     std::shuffle(deck.begin(), deck.end(), mt);
 }
 
+int total_hand(const hand_t& hand) {
+    int sum{};
+     for (const auto& card : hand) {
+        sum += get_card_value(card);
+    }
+    return sum;
+}
+
+void print_hand(const hand_t& hand) { 
+    for (const auto& card : hand) {
+        print_card(card);
+        std::cout << ' ';
+    }
+    std::cout << "\nTotal: " << total_hand(hand) << '\n';
+}
+
+const Card& draw(const deck_t& deck, size_t& top) {
+    return deck[top++];
+}
+ 
+void ignore_line() {
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+char get_player_action() {
+    char action{};
+    do {
+        std::cout << "\nHit or stand? (h/s) ";
+        std::cin >> action;
+        if (action == 'h' || action == 's') {
+            return action;
+        }
+        ignore_line();
+        std::cout << "Invalid input\n";
+    } while (true);
+}
+
+bool player_turn(const deck_t& deck, hand_t& hand, size_t& top) {
+    while (true) {
+        char action{get_player_action()};  
+        if (action == 's') {
+            return true;
+        }
+        hand.push_back(draw(deck, top));
+        print_hand(hand);
+        if (total_hand(hand) > blackjack) {
+            return false;
+        }
+    }
+}
+
+bool dealer_turn(const deck_t& deck, hand_t& hand, size_t& top) {
+    constexpr int done{17};
+    while (true) {
+        hand.push_back(draw(deck, top));
+        if (int total{total_hand(hand)}; total > done) {
+            print_hand(hand);
+            if (total > blackjack) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+bool play_blackjack(const deck_t& deck) {
+    size_t top{}; // current top of the deck
+    hand_t dealer{draw(deck, top)};
+    hand_t player{draw(deck, top), draw(deck, top)};
+
+    std::cout << "Player hand\n";
+    print_hand(player);
+    std::cout << "\nDealer hand\n";
+    print_hand(dealer);
+
+    std::cout << "\nPlayer's turn:\n";
+    if (!player_turn(deck, player, top)) { // player busted
+        std::cout << "Bust!\n";
+        return false;
+    }
+    std::cout << "\nDealer's turn:\n\n";
+    if (!dealer_turn(deck, dealer, top)) { // dealer busted
+        std::cout << "Dealer bust!\n";
+        return true;
+    }
+    return (total_hand(player) > total_hand(dealer));
+}
+
 int main() {
     deck_t deck{create_deck()};
-    print_deck(deck);
     shuffle_deck(deck);
-    print_deck(deck);
+    if (play_blackjack(deck)) {
+        std::cout << "You win!\n";
+    } else {
+        std::cout << "You lose!\n";
+    }
+
     return 0;
 }
