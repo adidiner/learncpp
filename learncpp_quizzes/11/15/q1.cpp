e891a71c608313bd842bbfed4be39c8542b3414a
Adi Dinerstein <adi.dinerstein@gmail.com>
2021-04-06 13:00:29 +0300

    chapter 11.15 quiz q1b

--- learncpp_quizzes/11/15/q1.cpp
+++ learncpp_quizzes/11/15/q1.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 
-class Vector3d {
-private:
-    double m_x{};
-    double m_y{};
-    double m_z{};
- 
-public:
-    Vector3d(double x = 0.0, double y = 0.0, double z = 0.0)
-        : m_x{x}, m_y{y}, m_z{z} {}
- 
-    void print() const {
-        std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-    }
-
-    friend class Point3d;
-};
+class Vector3d;
 
 class Point3d {
 private:
@@ -31,15 +16,33 @@ public:
         std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
     }
  
-    void moveByVector(const Vector3d &v) {
+    void moveByVector(const Vector3d &v);
+};
+
+class Vector3d {
+private:
+    double m_x{};
+    double m_y{};
+    double m_z{};
+ 
+public:
+    Vector3d(double x = 0.0, double y = 0.0, double z = 0.0)
+        : m_x{x}, m_y{y}, m_z{z} {}
+ 
+    void print() const {
+        std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+    }
+
+    friend void Point3d::moveByVector(const Vector3d &v);
+};
+
+void Point3d::moveByVector(const Vector3d &v) {
         m_x += v.m_x;
         m_y += v.m_y;
         m_z += v.m_z;
-    }
-};
+}
 
-int main()
-{
+int main() {
     Point3d p{1.0, 2.0, 3.0};
     Vector3d v{2.0, 2.0, -3.0};
  
